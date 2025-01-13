e283f4baddf53b794b55ca6e0ffccea5f8084300
Ángela Sofía <angela17.motta@gmail.com>
2025-01-13 16:25:00 -0500

    creación de la estructura

---
+++ index.js
@@ -0,0 +1,13 @@
+import  express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app= express();
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(morgan('dev'));
+
+app.listen(3000, ()=>{
+    console.log('Server listening on port 3000');
+});
