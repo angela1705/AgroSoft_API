c33fc667bea0055760798b36ed2cb4d80548fcc8
Haisonleandro <haisonleandrotoro@gmail.com>
2025-02-12 13:47:06 -0500

    arreglamos el json wed token de los routers

--- modulos/cultivo/router/router.tipo_actividad.js
+++ modulos/cultivo/router/router.tipo_actividad.js
@@ -1,11 +1,10 @@
 import verificarToken from "../../usuarios/middlewares/verificarToken.js"
-import { Router } from "express";
 import { postTipo_actividad, getTipo_actividad, getIdTipo_actividad, updateTipo_actividad } from "../controller/controller.tipo_actividad.js";
 const RouterTipo_actividad = Router();
 
-RouterTipo_actividad.post(verificarToken, "/tipo_actividad", postTipo_actividad);
-RouterTipo_actividad.get(verificarToken, "/tipo_actividad", getTipo_actividad);
-RouterTipo_actividad.get(verificarToken, "/tipo_actividad/:id", getIdTipo_actividad);
-RouterTipo_actividad.put(verificarToken, "/tipo_actividad/:id", updateTipo_actividad);
+RouterTipo_actividad.post("/tipo_actividad",verificarToken, postTipo_actividad);
+RouterTipo_actividad.get("/tipo_actividad",verificarToken, getTipo_actividad);
+RouterTipo_actividad.get("/tipo_actividad/:id",verificarToken, getIdTipo_actividad);
+RouterTipo_actividad.put("/tipo_actividad/:id",verificarToken, updateTipo_actividad);
 
 export default RouterTipo_actividad;
--- modulos/cultivo/router/router.tipo_especie.js
+++ modulos/cultivo/router/router.tipo_especie.js
@@ -1,11 +1,10 @@
 import verificarToken from "../../usuarios/middlewares/verificarToken.js"
-import { Router } from "express";
 import { postTipo_especie, getTipo_especie, getIdTipo_especie, updateTipo_especie } from "../controller/controller.tipo_especie.js";
 const RouterTipo_especie = Router();
 
-RouterTipo_especie.post(verificarToken, "/tipo_especie", postTipo_especie);
-RouterTipo_especie.get(verificarToken, "/tipo_especie", getTipo_especie);
-RouterTipo_especie.get(verificarToken, "/tipo_especie/:id", getIdTipo_especie);
-RouterTipo_especie.put(verificarToken, "/tipo_especie/:id", updateTipo_especie);
+RouterTipo_especie.post("/tipo_especie",verificarToken, postTipo_especie);
+RouterTipo_especie.get("/tipo_especie",verificarToken, getTipo_especie);
+RouterTipo_especie.get("/tipo_especie/:id",verificarToken, getIdTipo_especie);
+RouterTipo_especie.put("/tipo_especie/:id",verificarToken, updateTipo_especie);
 
 export default RouterTipo_especie;
