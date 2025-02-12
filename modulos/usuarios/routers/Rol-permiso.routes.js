e191cc6fead69e3c7f0f5625fdfd62b220733f44
GIOVANYSTIVEN <steventu06@gmail.com>
2025-02-12 18:13:14 -0500

    corrección de usuarios por super usuario y otros errores

--- modulos/usuarios/routers/Rol-permiso.routes.js
+++ modulos/usuarios/routers/Rol-permiso.routes.js
@@ -1,15 +1,15 @@
-import { Router } from 'express';
-import {
-  listarPermisosRol,
-  AsignarPermisoRol,
-  EliminarPermisoRol
+import express from 'express';
+import { 
+  listarPermisosRol, 
+  AsignarPermisoRol, 
+  EliminarPermisoRol 
 } from '../controllers/RolPermiso.controllers.js';
 import verificarToken from '../middlewares/verificarToken.js';
 
-const router = Router();
+const router = express.Router();
 
-router.get('/rol-permisos', listarPermisosRol);
-router.post('/rol-permisos', verificarToken, AsignarPermisoRol);
-router.delete('/rol-permisos/:id', verificarToken, EliminarPermisoRol);
+router.get('/rol-permiso', listarPermisosRol);
+router.post('/rol-permiso', verificarToken, AsignarPermisoRol);
+router.delete('/rol-permiso/:fk_rol/:fk_permiso', verificarToken, EliminarPermisoRol);
 
 export default router;
