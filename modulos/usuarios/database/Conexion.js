1a6ebe56d9b171eed080efe5c3336c7eeba60f02
David <Juandavides120@gmail.com>
2025-02-15 22:55:00 -0500

    restaruracion de password

--- modulos/usuarios/database/Conexion.js
+++ modulos/usuarios/database/Conexion.js
@@ -8,7 +8,7 @@ const { Pool } = pg;
 const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '1234',
+  password: process.env.DB_PASSWORD || 'adso2024',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_DATABASE || 'dbagro',
 });
